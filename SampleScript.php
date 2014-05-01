8ff4b8bd00a6d3583e0bcfcff80b0701ca11053e
viveksg <vivek.rdso@gmail.com>
2014-05-01 18:46:16 +0530

    Added SampleScript.php

---
+++ SampleScript.php
@@ -0,0 +1,13 @@
+<?php
+$val_key=$_REQUEST['val_key'];
+$ver=$_REQUEST['version'];
+$latest_version=2;//here $latest_version is hardcoded but you can store it in DB and fetch it at runtime.
+$data="link_to_APK_file_of_latest_version";
+if($val_key=="your_key")
+echo "$latest_version</br>$data";
+/*This is just a simple example you can
+ store version,link and key values in some database
+ and script can fetch them while execution and do the
+Key check and provide the link*/
+?>
+
