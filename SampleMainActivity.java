6677a456631eb54a5566a85aca1688c13ec6eaca
viveksg <vivek.rdso@gmail.com>
2014-05-01 18:06:48 +0530

    Added SampleAndroidManifest.xml , Updates SampleMainActivity.java

--- SampleMainActivity.java
+++ SampleMainActivity.java
@@ -8,9 +8,9 @@ import com.lib.DNILib.UpdateCheckBroadcastReceiver;
 import com.lib.DNILib.Manager;
 import com.lib.DNILib.CheckUpdateAndDownloadService;
 import com.lib.DNILib.DownloadBroadcastReceiver;
-public class SampleMainActivity extends Activity
+public class MainActivity extends Activity
 {
-  String link="your_app_update_check_link";
+  String link="link to your app";
    UpdateCheckBroadcastReceiver ucbr=null;
    DownloadBroadcastReceiver dbr=null;
    Manager manager=null;
@@ -19,26 +19,34 @@ public class SampleMainActivity extends Activity
     {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.main);
-        try{
+        try{ //add val_key parameter with value to URL
         link=link+"?val_key="+URLEncoder.encode("your_key","UTF-8");
+
         }
         catch(Exception e){}
+        //Instantiate Manager class
         manager=new Manager(this);
+        // Set thhe update check link for manager objet
         manager.setUpdateCheckURL(link);
+        //Instantiate UpdateCheckBroadcastReceiver class
         ucbr=new UpdateCheckBroadcastReceiver(manager);
+         //Instantiate DownloadBroadcastReceiver class
         dbr= new DownloadBroadcastReceiver(manager);
 
     }
     @Override
     public void onStart()
     {   super.onStart();
+        //Register Listeners
         registerReceiver(ucbr,new IntentFilter(CheckUpdateAndDownloadService.UPDATE_FOUND));
         registerReceiver(dbr,new IntentFilter(CheckUpdateAndDownloadService.DOWNLOAD_COMPLETE));
+        // Call manager.Checkupdates() to initiate update check service
         manager.checkUpdates();
     }
     @Override
      public void onDestroy()
     {   super.onDestroy();
+        //Unregister receivers
         unregisterReceiver(ucbr);
         unregisterReceiver(dbr);
 
