d9b33adc3781a991aa34c336106748d76827ed5f
Vivek Singh <vivek.rdso@gmail.com>
2014-05-01 18:59:46 +0530

    Update SampleScript.php

--- SampleScript.php
+++ SampleScript.php
@@ -1,7 +1,7 @@
 <?php
 $val_key=$_REQUEST['val_key'];
 $ver=$_REQUEST['version'];
-$latest_version=2;//here $latest_version is hardcoded but you can store it in DB and fetch it at runtime.
+$latest_version=2;//here $latest_version is hardcoded but you can store latest version  in DB and fetch it at runtime.
 $data="link_to_APK_file_of_latest_version";
 if($val_key=="your_key")
 echo "$latest_version</br>$data";
